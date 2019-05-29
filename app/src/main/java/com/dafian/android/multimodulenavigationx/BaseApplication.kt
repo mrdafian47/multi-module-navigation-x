998df5251e948895f75ee6efaf5983f4b825f98e
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2019-05-29 10:39:04 +0700

    Add koin

---
+++ app/src/main/java/com/dafian/android/multimodulenavigationx/BaseApplication.kt
@@ -0,0 +1,16 @@
+package com.dafian.android.multimodulenavigationx
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@BaseApplication)
+        }
+    }
+}
