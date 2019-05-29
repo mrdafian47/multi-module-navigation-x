0fdc332613218507c7313bfc318a3a5bd5d1cc33
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2019-05-29 09:51:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/dafian/android/multimodulenavigationx/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dafian.android.multimodulenavigationx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
