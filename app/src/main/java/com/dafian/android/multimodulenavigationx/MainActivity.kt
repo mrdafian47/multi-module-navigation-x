3aa2a520d80b0872d031f0b21f11d8d4d60752b4
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2019-05-29 10:33:28 +0700

    Fix Bottom Navigation Route

--- app/src/main/java/com/dafian/android/multimodulenavigationx/MainActivity.kt
+++ app/src/main/java/com/dafian/android/multimodulenavigationx/MainActivity.kt
@@ -2,11 +2,22 @@ package com.dafian.android.multimodulenavigationx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottom_navigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_graph).navigateUp()
     }
 }
